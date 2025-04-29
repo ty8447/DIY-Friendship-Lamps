16e81896482c6fa700186b12de9b6ed3612ab0ae
ty8447 <32338679+ty8447@users.noreply.github.com>
2025-04-28 23:42:29 -0400

    Update Lamp_Device_V4.ino
    Added Reset Password State on code check

--- LampClient/Lamp_Device_V4.ino
+++ LampClient/Lamp_Device_V4.ino
@@ -57,9 +57,9 @@ char mqtt_Username[40];
 char mqtt_Password[40];
 const char* filename = "/mqtt_Server";
 const int mqttPort = 1883;
-const char* clientId = "ESP32_2";  // Change for other ESP32
-const char* publishTopic = "esp32/ESP32_2/data";
-const char* subscribeTopic = "esp32/ESP32_1/data";
+const char* clientId = "ESP32_1";  // Change for other ESP32
+const char* publishTopic = "esp32/ESP32_1/data";
+const char* subscribeTopic = "esp32/ESP32_2/data";
 bool canAcceptMessage = true;
 String apName = "Lamp ";
 char* msgTypeOp[4] = { "Send", "Confirm", "Reply", "Secret" };
@@ -430,6 +430,7 @@ void checkColorCode() {
       patterns(0);
     }
     cCodeFull = "";
+    cCodeMode = false;
     cCodeCount = 0;
   }
 }
